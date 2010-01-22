f0ace022927a4f99d8a57245c9eef0d13f21e539
sgtcoolguy <cwilliams@aptana.com>
2010-01-22 13:24:41 -0500

    massage to work in block and add FIXME marker about tooltip not showing when there's a selection

--- commands/word_count.rb
+++ commands/word_count.rb
@@ -1,24 +1,22 @@
 require 'radrails'
 
+def pretty(number)
+  number.to_s.gsub(/\d{1,3}(?=\d{3}+(?!\d))/, '\0,')
+end
+# FIXME Tooltip isn't showing properly when input is selection...
 command 'Statistics for Document / Selection (Word Count)' do |cmd|
   cmd.key_binding = 'CONTROL+M2+N'
   cmd.output = :show_as_tooltip
   cmd.input = :selection, :document
-  cmd.invoke =<<-EOF
-#!/usr/bin/env ruby -wKU
-
-def pretty(number)
-  number.to_s.gsub(/\d{1,3}(?=\d{3}+(?!\d))/, '\0,')
-end
-
-counts = `wc -lwc`.scan(/\d+/)
-counts[0] = counts[0].to_i + 1 # increase one to the line count
-
-%w[ line word byte ].each do |unit|
-  cnt    = counts.shift
-  plural = cnt.to_i != 1 ? 's' : ''
-  printf("%11.11s %s%s\n", pretty(cnt), unit, plural)
-end
-
-EOF
+  cmd.invoke do
+    counts = IO.popen('wc -lwc', 'r+') { |io| io.write STDIN.read; io.close_write; io.read }.scan(/\d+/)
+    counts[0] = counts[0].to_i + 1 # increase one to the line count
+    
+    %w[ line word byte ].each do |unit|
+      cnt    = counts.shift
+      plural = cnt.to_i != 1 ? 's' : ''
+      printf("%11.11s %s%s\n", pretty(cnt), unit, plural)
+    end
+    nil
+  end
 end
