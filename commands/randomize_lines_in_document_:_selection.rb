9057b00a2cfd7690e656d21529d40506f7085e54
sgtcoolguy <cwilliams@aptana.com>
2010-01-22 14:12:35 -0500

    fix to use fallback input of document

--- commands/randomize_lines_in_document_:_selection.rb
+++ commands/randomize_lines_in_document_:_selection.rb
@@ -2,7 +2,7 @@ require 'radrails'
 # FIXME Won't work on Windows
 command 'Randomize Lines in Document / Selection' do |cmd|
   cmd.output = :replace_selection
-  cmd.input = :selection
+  cmd.input = :selection, :document
   cmd.invoke =<<-EOF
 ruby -pe'$_ = (rand * 100000000).round.to_s + "\t" + $_'|sort|cut -f2-
 EOF
