a2ac3543b9c986495c50802a269cb07a67f8c15d
sgtcoolguy <cwilliams@aptana.com>
2010-01-22 13:49:13 -0500

    Convert perl execution via shell to ruby regexp replacement inside block

--- commands/strip_non-printables.rb
+++ commands/strip_non-printables.rb
@@ -1,9 +1,10 @@
 require 'radrails'
-# FIXME Convert perl execution to ruby regex replacement
+
 command 'Remove Unprintable Characters in Document / Selection' do |cmd|
   cmd.output = :replace_selection
   cmd.input = :selection, :document
-  cmd.invoke =<<-EOF
-perl -pe 's/[^\t\n\x20-\xFF]|\x7F|\xC2[\x80-\x9F]//g'
-EOF
+  cmd.invoke do
+    STDIN.readlines.each {|l| print l.gsub(/[^\t\n\x20-\xFF]|\x7F|\xC2[\x80-\x9F]/, '') }
+    nil
+  end
 end
