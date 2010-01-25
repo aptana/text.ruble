c3b0421a7b36eb5ba98a15d045e835b083f6b62c
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 11:31:30 -0500

    fix input type of character to become right_character

--- commands/convert_to_hex.rb
+++ commands/convert_to_hex.rb
@@ -3,7 +3,7 @@ require 'radrails'
 command 'Convert Character / Selection to Hex' do |cmd|
   cmd.key_binding = 'CONTROL+M2+X'
   cmd.output = :replace_selection
-  cmd.input = :selection, :character
+  cmd.input = :selection, :right_character
   cmd.invoke do
     print STDIN.read.unpack("U*").map { |e| sprintf('0x%02x', e) }.join(' ')
   end
