f084666b71b4904407c6611cc94f005dff1e48e9
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 14:03:24 -0500

    use fall back input of :document

--- commands/sort_&_uniq.rb
+++ commands/sort_&_uniq.rb
@@ -3,7 +3,7 @@ require 'radrails'
 command 'Sort Lines & Remove Duplicates' do |cmd|
   cmd.key_binding = 'F5'
   cmd.output = :replace_selection
-  cmd.input = :selection
+  cmd.input = :selection, :document
   cmd.invoke do 
     STDIN.readlines.sort {|a,b| a.downcase <=> b.downcase }.uniq.join("")
   end
