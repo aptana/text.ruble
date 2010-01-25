e36eabb35c17908a0ca66b02171768e6e0ed234a
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 11:50:25 -0500

    convert commands to work in RadRails via block execution

--- commands/copy_non-matching_lines_into_new_document.rb
+++ commands/copy_non-matching_lines_into_new_document.rb
@@ -1,26 +1,14 @@
 require 'radrails'
 
 command 'Copy Non-Matching Lines into New Document' do |cmd|
-  cmd.output = :open_as_new_document
-  cmd.input = :selection
-  cmd.invoke =<<-EOF
-res=$(ruby -rui -e"print TextMate::UI.request_string(:title => 'Filter Non-Matching Lines', :prompt => 'Enter a pattern:', :button1 => 'Filter', :button2 => 'Cancel').to_s")
-
-[[ -z "$res" ]] && exit_discard
-export pattern="$res"
-
-# This could be done with grep, but Python's RE is closer to oniguruma
-"${TM_PYTHON:-python}" -c '
-import sys, os, re, traceback
-try:
-  pattern = re.compile(os.environ["pattern"])
-except re.error, e:
-  sys.stderr.write("Invalid pattern: %s" % e)
-  sys.exit(1)
-for line in sys.stdin:
-  if not pattern.search(line):
-    sys.stdout.write(line)
-' || exit_show_tool_tip
-
-EOF
+  cmd.output = :create_new_document
+  cmd.input = :document
+  cmd.invoke do |context|
+    res = RadRails::UI.request_string(:title => 'Filter Non-Matching Lines', :prompt => 'Enter a pattern:', :button1 => 'Filter', :button2 => 'Cancel')
+    context.exit_discard unless res
+    res = res.to_s
+    # Now go through every line in STDIN and print all non-matches!
+    STDIN.readlines.each {|line| puts line if !line.match(res) }
+    nil
+  end
 end
