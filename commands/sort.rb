a88ee1b279d4f58b4d303d380866a8688f50c757
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 11:31:08 -0500

    convert from sing 'sort' and 'uniq' command line utils to using pure ruby so we can work on Windows.

--- commands/sort.rb
+++ commands/sort.rb
@@ -1,10 +1,10 @@
 require 'radrails'
-# FIXME Won't work on Windows
+# FIXME used to run "sort -f" which sorts things like [ after a-z, but ruby sorts before a-z
 command 'Sort Lines in Document / Selection' do |cmd|
   cmd.key_binding = 'F5'
   cmd.output = :replace_selection
   cmd.input = :selection, :document
-  cmd.invoke =<<-EOF
-sort -f
-EOF
+  cmd.invoke do 
+    STDIN.readlines.sort {|a,b| a.downcase <=> b.downcase }.join("")
+  end
 end
