34f2f080b73dce0be9c9877ff73f507d74fdcef4
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 12:19:26 -0500

    convert to pure ruby so it'll work on Windows

--- commands/randomize_lines_in_document_:_selection.rb
+++ commands/randomize_lines_in_document_:_selection.rb
@@ -1,9 +1,10 @@
 require 'radrails'
-# FIXME Won't work on Windows
+
 command 'Randomize Lines in Document / Selection' do |cmd|
   cmd.output = :replace_selection
   cmd.input = :selection, :document
-  cmd.invoke =<<-EOF
-ruby -pe'$_ = (rand * 100000000).round.to_s + "\t" + $_'|sort|cut -f2-
-EOF
+  cmd.invoke do
+    STDIN.readlines.sort { rand(3) - 1 }.join('')
+    #STDIN.readlines.sort_by { rand }.join('')
+  end
 end
