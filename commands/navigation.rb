b145788a05cf5f13cfc6d5bf6852704ec453cc9e
Sandip Chitale <schitale@aptana.com>
2010-02-26 11:20:38 -0800

    Add Text > Navigation menu and also add Next Editor (CTRL+TAB) and Previous Editor (CTRL+SHIFT+TAB) Commands.

---
+++ commands/navigation.rb
@@ -0,0 +1,13 @@
+command "Previous Editor" do |cmd|
+  cmd.key_binding = "CTRL+SHIFT+TAB"
+  cmd.output = :discard
+  cmd.input = :none
+  cmd.invoke { Ruble::UI.previous_editor }
+end
+
+command "Next Editor" do |cmd|
+  cmd.key_binding = "CTRL+TAB"
+  cmd.output = :discard
+  cmd.input = :none
+  cmd.invoke { Ruble::UI.next_editor }
+end
