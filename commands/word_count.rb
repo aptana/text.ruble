c3fcedaa0ae33f034279faecba617e27b5270622
Allen Yeung <ayeung@aptana.com>
2010-03-29 13:29:25 -0700

    Fixed 'Word Count' command to work for windows

--- commands/word_count.rb
+++ commands/word_count.rb
@@ -4,16 +4,28 @@ def pretty(number)
   number.to_s.gsub(/\d{1,3}(?=\d{3}+(?!\d))/, '\0,')
 end
 # FIXME Tooltip isn't showing properly when input is selection...
-# FIXME Won't work on windows, should convert to pure ruby if possible (see http://dada.perl.it/shootout/wc.ruby.html)
 command 'Statistics for Document / Selection (Word Count)' do |cmd|
   cmd.key_binding = 'CONTROL+M2+N'
   cmd.output = :show_as_tooltip
   cmd.input = :selection, :document
-  cmd.invoke.windows = ""
   cmd.invoke do
-    counts = IO.popen('wc -lwc', 'r+') { |io| io.write STDIN.read; io.close_write; io.read }.scan(/\d+/)
-    counts[0] = counts[0].to_i + 1 # increase one to the line count
     
+   lines = words = bytes = 0
+   
+   loop do
+    data = (STDIN.read(4096) or break) << (STDIN.gets || "")
+    bytes += data.length
+    lines += data.count("\n")
+    ((data.strip! || data).gsub!(/[\n\r]/, " ") || data).squeeze!
+    words += data.count(" ") + 1
+   end
+
+    counts = []
+    
+    counts << (lines +1)
+    counts << words
+    counts << bytes
+
     %w[ line word byte ].each do |unit|
       cnt    = counts.shift
       plural = cnt.to_i != 1 ? 's' : ''
