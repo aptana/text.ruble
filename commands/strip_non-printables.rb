eb020d3a4a4e0eb24b79c24e5c83b068ccbd5a06
sgtcoolguy <cwilliams@aptana.com>
2010-04-08 13:45:00 -0400

    use $stdin so we can override for testing

--- commands/strip_non-printables.rb
+++ commands/strip_non-printables.rb
@@ -4,7 +4,7 @@ command 'Remove Unprintable Characters in Document / Selection' do |cmd|
   cmd.output = :replace_selection
   cmd.input = :selection, :document
   cmd.invoke do
-    STDIN.readlines.each {|l| print l.gsub(/[^\t\n\x20-\xFF]|\x7F|\xC2[\x80-\x9F]/, '') }
+    $stdin.readlines.each {|l| print l.gsub(/[^\t\n\x20-\xFF]|\x7F|\xC2[\x80-\x9F]/, '') }
     nil
   end
 end
