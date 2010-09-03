10544e949b77bb3baf1682db7815122cfc2cdafa
sgtcoolguy <cwilliams@aptana.com>
2010-09-03 13:12:32 -0400

    push F5 commands to F6

--- commands/sort_uniq.rb
+++ commands/sort_uniq.rb
@@ -1,7 +1,7 @@
 require 'ruble'
 # FIXME used to run "sort -f" which sorts things like [ after a-z, but ruby sorts before a-z
 command 'Sort Lines & Remove Duplicates' do |cmd|
-  cmd.key_binding = 'F5'
+  cmd.key_binding = 'F6' # To avoid clash with Eclipse F5
   cmd.output = :replace_selection
   cmd.input = :selection, :document
   cmd.invoke do 
